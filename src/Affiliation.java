926a58b215e7219d4535ed7bb9b9009fd3f880b3
palomaarize <palomaarize0811@gmail.com>
2025-01-31 10:57:40 -0300

    add affiliation

--- src/Affiliation.java
+++ src/Affiliation.java
@@ -1,3 +1,34 @@
-// TODO CRIAR CLASSE, GET/SET E CONSTRUCTOR
 public class Affiliation {
+    private String name;
+    private RelationshipType relationship;
+
+    public Affiliation() {
+    }
+
+    public Affiliation(String name, RelationshipType relationship) {
+        this.name = name;
+        this.relationship = relationship;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public RelationshipType getRelationship() {
+        return relationship;
+    }
+
+    public void setRelationship(RelationshipType relationship) {
+        this.relationship = relationship;
+    }
+
+    @Override
+    public String toString() {
+        return "Affiliation{name='" + name + "', relationship=" + relationship + "}";
+    }
 }
+

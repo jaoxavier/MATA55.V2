a3b8e37e0139ac9fdeeda6b8f37ec61b86c19576
igortpfalcao <rogianilorac@gmail.com>
2025-02-03 09:23:54 -0300

    get e set company

--- src/Company.java
+++ src/Company.java
@@ -34,7 +34,7 @@ public class Company extends Person{
         return false;
     }
 
-    protected int calcularDigito(String cnpj, int[] tabela){
+    private int calcularDigito(String cnpj, int[] tabela){
         int sum = 0;
         for (int i = 0; i < tabela.length; i++){
             sum += (cnpj.charAt(i) - '0') * tabela[i];
